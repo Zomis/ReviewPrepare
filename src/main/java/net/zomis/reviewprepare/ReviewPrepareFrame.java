488f7f81af80bade95914933c3cb597876c5ef3a
Janos Gyerik <janos@kronos>
2014-08-17 13:33:24 +0200

    removed unused imports

--- src/main/java/net/zomis/reviewprepare/ReviewPrepareFrame.java
+++ src/main/java/net/zomis/reviewprepare/ReviewPrepareFrame.java
@@ -14,9 +14,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 
 
