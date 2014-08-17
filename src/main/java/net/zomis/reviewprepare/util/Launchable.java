f5f26fae5b60933d409514a3d5db661da939dc27
JaDogg <jadogg.coder@gmail.com>
2014-08-17 18:30:44 +0530

    Whitespaces

--- src/main/java/net/zomis/reviewprepare/util/Launchable.java
+++ src/main/java/net/zomis/reviewprepare/util/Launchable.java
@@ -4,14 +4,16 @@ import javafx.stage.Stage;
 
 /**
  * Launch-able interface
+ *
  * @author Bhathiya
  */
 public interface Launchable {
 
     /**
      * launch a user interface
+     *
      * @param stage current stage
-     * @param parameters 
+     * @param parameters
      */
-    void launch(Stage stage,Object[] parameters);
+    void launch(Stage stage, Object[] parameters);
 }
