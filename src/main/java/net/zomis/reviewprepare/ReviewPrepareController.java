daba6b5aa49eec370a53abe41d683c636773d2b2
JaDogg <jadogg.coder@gmail.com>
2014-08-17 17:17:50 +0530

    Added FXML (Code Save)

---
+++ src/main/java/net/zomis/reviewprepare/ReviewPrepareController.java
@@ -0,0 +1,24 @@
+package net.zomis.reviewprepare;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Bhathiya
+ */
+public class ReviewPrepareController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
